a1d4e6ef420218a953c79f638dc68fbbdfe7e216
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2021-03-14 10:19:50 +0000

    [server] refactor: Compliant with dotenv notation

--- packages/server/src/modules/config/config.service.ts
+++ packages/server/src/modules/config/config.service.ts
@@ -39,7 +39,7 @@ export class ConfigService {
    * @param env The environment name. Corresponding `name.env` file will be used. Default to `local`
    */
   private getConfigFromEnvFile(env = 'local'): DotenvParseOutput {
-    const envFilePath = join('env', `${env}.env`);
+    const envFilePath = join('env', `.env.${env}`);
     try {
       const config = parse(readFileSync(envFilePath));
       return config;
