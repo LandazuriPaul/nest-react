f688a0b3ae16da83198e6daa62387df39926582f
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2022-08-21 09:58:55 +0100

    fix(server): config service call in main.ts

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 
 import { APIModule } from './modules/api.module';
+import { ConfigService } from './modules/config/config.service';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(APIModule);
-  const config = app.get('ConfigService').envConfig;
-  app.enableCors({ origin: config.CORS_WHITELIST.split(',') });
-  await app.listen(config.PORT);
+  const configService = app.get(ConfigService);
+  app.enableCors({ origin: configService.corsWhiteList });
+  await app.listen(configService.port);
 }
 
 bootstrap();
